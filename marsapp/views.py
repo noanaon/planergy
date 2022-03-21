da69730c4606eb27e88b4ca4548d826a99c593e2
noa <noana.smits@gmail.com>
2022-03-21 10:15:03 +0100

    Colony page, room page, device page added

--- marsapp/views.py
+++ marsapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'marsapp/home.html')
+
+def colony(request):
+    return render(request, 'marsapp/colony.html')
+
+def livingroom(request):
+    return render(request, 'marsapp/livingroom.html')
